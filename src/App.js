e59361a0e3d8166a23310f2328d63372336238ee
waliddb1997 <waliddabbaghi5@gmail.com>
2023-01-16 16:13:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import { Col, Row, Card, Form } from "react-bootstrap";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">ReactJS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            
+            
+          
+          </Nav>
+         
+        </Navbar.Collapse>
+        
+      </Container>
+      <Button variant="light">Login</Button>{' '}
+      <Button variant="light">sign up</Button>{' '}
+    </Navbar> 
+
+
+    <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <div className="border border-3 border-primary"></div>
+            <Card className="shadow">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 text-uppercase ">Walid</h2>
+                  <p className=" mb-5">Please enter your login and password!</p>
+                  <div className="mb-3">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Email address
+                        </Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicCheckbox"
+                      >
+                        <p className="small">
+                          <a className="text-primary" href="#!">
+                            Forgot password?
+                          </a>
+                        </p>
+                      </Form.Group>
+                      <div className="d-grid">
+                        <Button variant="primary" type="submit">
+                          Login
+                        </Button>
+                      </div>
+                    </Form>
+                    <div className="mt-3">
+                      <p className="mb-0  text-center">
+                        Don't have an account?{" "}
+                        <a href="{'Login'}" className="text-primary fw-bold">
+                          Sign Up
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      <div className="border border-3 border-primary"></div>
+
+      <footer  className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            
+
+            
+        </div>
     </div>
+
+    <div  className="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> Walid Dabbaghi</a>
+    </div>
+
+</footer>
+    
+
+  
+    </>
   );
 }
 
